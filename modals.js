86b013f0942d6a2ef9474887d3064d1eb2a99813
mihaiarthas <mihaifnitu@gmail.com>
2017-11-03 22:12:34 +0200

    Add files via upload

--- modals.js
+++ modals.js
@@ -1,25 +1,6 @@
-var modal = document.getElementById('modal-1');
-var btn = document.getElementById("btn-1");
-var span = document.getElementById("close-1");
-
-// var modal = document.getElementById('modal-2');
-// var btn = document.getElementById("btn-2");
-// var span = document.getElementById("close-2");
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 function setModalWidth(array, value) {
-    for(i = 0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
         array[i].style.width = value;
     }
 }
@@ -28,16 +9,50 @@ function checkViewWidth() {
     var resWidth = window.innerWidth;
     var modArray = document.getElementsByClassName('modal-content');
     var width = '';
-    if(resWidth <= 360) {
+    if (resWidth <= 360) {
         width = '100%';
     }
-    else if(resWidth > 360 && resWidth <= 1200) {
-        width = '90%';        
+    else if (resWidth > 360 && resWidth <= 1200) {
+        width = '90%';
     }
     else {
         width = '50%';
     }
-    this.setModalWidth(modArray, width);               
+    this.setModalWidth(modArray, width);
+}
+
+var modal = new Object();
+var btn = new Object();
+var span = new Object();
+
+function openModal(id) {
+    this.modal = document.getElementById('modal-' + id);
+    this.btn = document.getElementById('btn-' + id);
+    this.span = document.getElementById('close-' + id);
+
+    modal.style.display = "block";
+
+    if(this.isNullOrUndefined(span.onclick)) {
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+    }
+
+    if(this.isNullOrUndefined(window.onclick)) {
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
+ 
+}
+
+function isNullOrUndefined(object) {
+    if(object === null || object === undefined) {
+        return true;
+    }
+    return false;
 }
 
 
